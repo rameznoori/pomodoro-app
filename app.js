1cf73c8964602e749a5b20bec328587d1296285c
rameznoori <199920071+rameznoori@users.noreply.github.com>
2025-09-09 16:51:21 +0530

    declared variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const bells = new Audio('./sounds/bell.wav');
+const start_btn = document.querySelector('.btn-start');
+const session = document.querySelector('.minutes');
+let my_interval;
+let state = true;
+
